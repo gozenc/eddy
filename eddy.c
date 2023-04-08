3028c72edbe170e08be294f5a5a3a19efefaceb2
Fatih Gozenc <fatihgozenc@gmail.com>
2023-04-08 20:25:51 +0200

    added pageup page down support

--- eddy.c
+++ eddy.c
@@ -10,6 +10,15 @@
 #define EDDY_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 
+enum editorKey {
+  ARROW_LEFT = 1000,
+  ARROW_RIGHT,
+  ARROW_UP,
+  ARROW_DOWN,
+  PAGE_UP,
+  PAGE_DOWN,
+};
+
 struct editorConfig {
 	int cx, cy, screenrows, screencols;
 	struct termios orig_termios;
@@ -55,7 +64,7 @@ void enableRawMode(void) {
 	}
 }
 
-char editorReadKey(void) {
+int editorReadKey(void) {
 	int nread;
 	char c;
 	while ( (nread = read(STDIN_FILENO, &c, 1)) != 1 ) {
@@ -69,11 +78,22 @@ char editorReadKey(void) {
     if ( read(STDIN_FILENO, &seq[1], 1) != 1 ) return '\x1b';
 
     if ( seq[0] == '[' ) {
-      switch (seq[1]) {
-        case 'A': return 'w';
-        case 'B': return 's';
-        case 'C': return 'd';
-        case 'D': return 'a';
+
+      if ( seq[1] >= '0' && seq[0] <= '9') {
+        if ( read(STDIN_FILENO, &seq[2], 1) != 1 ) return '\x1b';
+        if ( seq[2] == '~' ) {
+          switch (seq[1]) {
+            case '5': return PAGE_UP;
+            case '6': return PAGE_DOWN;
+          }
+        }
+      } else {
+        switch (seq[1]) {
+          case 'A': return ARROW_UP;
+          case 'B': return ARROW_DOWN;
+          case 'C': return ARROW_RIGHT;
+          case 'D': return ARROW_LEFT;
+        }
       }
     }
 
@@ -189,25 +209,33 @@ void editorRefreshScreen(void) {
 }
 
 /* INPUT */
-void editorMoveCursor(char key) {
+void editorMoveCursor(int key) {
   switch (key) {
-    case 'a':
-      E.cx--;
+    case ARROW_LEFT:
+      if ( E.cx != 0 ) {
+        E.cx--;
+      }
       break;
-    case 'd':
-      E.cx++;
+    case ARROW_RIGHT:
+      if ( E.cx != E.screencols - 1 ) {
+        E.cx++;
+      }
       break;
-    case 'w':
-      E.cy--;
+    case ARROW_UP:
+      if ( E.cy != 0 ) {
+        E.cy--;
+      }
       break;
-    case 's':
-      E.cy++;
+    case ARROW_DOWN:
+      if ( E.cy != E.screenrows - 1) {
+        E.cy++;
+      }
       break;
   }
 }
 
 void editorProcessKeyPress(void) {
-	char c = editorReadKey();
+	int c = editorReadKey();
 	
 	switch (c) {
 		case CTRL_KEY('q'):
@@ -215,10 +243,18 @@ void editorProcessKeyPress(void) {
 			exit(0);
 			break;
 
-    case 'w':
-    case 'a':
-    case 's':
-    case 'd':
+    case PAGE_UP:
+    case PAGE_DOWN:
+      {
+        int times = E.screenrows;
+        while (times--)
+          editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+      }
+      break;
+    case ARROW_UP:
+    case ARROW_DOWN:
+    case ARROW_LEFT:
+    case ARROW_RIGHT:
       editorMoveCursor(c);
       break;
 	}
